f4d349af50d69d1162389b8a29ca4e72aac5c967
Kaden Osborn <kadenosborn2019@gmail.com>
2026-06-15 22:24:55 -0500

    added the ability to check directories for files and give name, size, and weather or not its a directory to the get_files_info funct. updated the tests for the new functionality. Started a new feature for parsing the files after info is gathered

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -6,8 +6,16 @@ def get_files_info(working_directory: str, directory: str = ".") -> str | None:
     valid_target = os.path.commonpath([os.path.abspath(working_directory), target_dir]) == os.path.abspath(working_directory)
     if not valid_target:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if not os.path.isdir(directory):
+    if not os.path.isdir(target_dir):
         return f'Error: "{directory}" is not a directory'
     if valid_target:
-        return f'Success: "{directory}" is within the working directory'
+        if os.path.isdir(target_dir):
+            files_info = []
+            for file in os.listdir(target_dir):
+                file_path = os.path.join(target_dir, file)
+                file_size = (os.path.getsize(file_path))
+                is_a_file = (os.path.isdir(file_path))
+                formated_info = f"- {file}: file_size={file_size}, is_dir={is_a_file}"
+                files_info.append(formated_info)
+            return "\n".join(files_info)
     return None
